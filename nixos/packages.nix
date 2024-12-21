ffef8836649ac7160c3a272d2d4f181a50923c1a
RURURURU <68574266+rubberducky8601@users.noreply.github.com>
2024-12-20 22:11:20 -0500

    Update packages.nix

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -14,14 +14,17 @@
 	wget
 	screenfetch
 	lazygit
+	github-desktop
+  	google-chrome
 	
 	#coding
-	quartus-prime-lite
+	#quartus-prime-lite
 	jdk21_headless
+	
 	];
 
-	 environment.variables.LM_LICENSE_FILE = "/home/ru/Documents/LR-194472_License.dat";
-  	 environment.sessionVariables.LM_LICENSE_FILE = "/home/ru/Documents/LR-194472_License.dat";
+	 #environment.variables.LM_LICENSE_FILE = "/home/ru/Documents/LR-194472_License.dat";
+  	 #environment.sessionVariables.LM_LICENSE_FILE = "/home/ru/Documents/LR-194472_License.dat";
 
 	fonts.packages = with pkgs; [
 	jetbrains-mono
